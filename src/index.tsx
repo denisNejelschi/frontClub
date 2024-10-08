dcc9d6287775d28f597926e79fc46ca7265bb2e5
Versina Vasile <ipvvg4@gmail.com>
2024-10-08 20:50:29 +0200

    add kore auth

--- src/index.tsx
+++ src/index.tsx
@@ -1,50 +1,21 @@
-import ReactDOM from "react-dom/client";
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
-import { UserProvider } from "./components/userContext/UserContext";
-import Layout from "./components/layout/Layout";
-import Register from "./components/auth/Register";
-import Login from "./components/auth/Login";
-import ActivityList from "./components/activityList/ActivityList";
-import HomePage from "./components/homePages/HomePage";
-import School from "./components/school/school";
-import ProtectedRoute from "./components/protectedRoute/ProtectedRoute";
-import AddActivityForm from "./components/addActivitiesForm/AddActivitiesForm";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const container = document.getElementById("root");
 
-root.render(
-  <Provider store={store}>
-    <UserProvider>
-      <HashRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<HomePage />} />
-            <Route path="/homePage" element={<HomePage />} />
-            <Route path="/activityList" element={<ActivityList />} />
-            <Route
-              path="/activityList/addActivity"
-              element={
-                <AddActivityForm
-                  onSuccess={function (): void {
-                    throw new Error("Function not implemented.");
-                  }}
-                />
-              }
-            />
-            <Route
-              path="/school"
-              element={<ProtectedRoute component={<School />} />}
-            />
-            <Route path="login" element={<Login />} />
-            <Route path="register" element={<Register />} />
-            <Route path="*" element={<h1>Error 404 😵</h1>} />
-          </Route>
-        </Routes>
-      </HashRouter>
-    </UserProvider>
-  </Provider>
-);
+if (container) {
+  const root = createRoot(container);
+
+  root.render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+} else {
+  throw new Error(
+    "Root element with ID 'root' was not found in the document. Ensure there is a corresponding HTML element with the ID 'root' in your HTML file."
+  );
+}
